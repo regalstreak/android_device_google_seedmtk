a02746f3d613cab6f266a0afff1493bfe8e04c77
MSF Jarvis <msfjarvis@gmail.com>
2016-06-23 17:21:23 +0530

    seedmtk-ify
    Signed-off-by: MSF Jarvis <msfjarvis@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
-$(call inherit-product, device/google/sprout8/sprout8.mk)
+$(call inherit-product, device/google/seedmtk/seedmtk.mk)
 
 # Common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_sprout8
-PRODUCT_DEVICE :=sprout8
+PRODUCT_NAME := cm_seedmtk
+PRODUCT_DEVICE :=seedmtk
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
-PRODUCT_MODEL := A1
+PRODUCT_MODEL := Pixelv1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="sprout8"
+    PRODUCT_DEVICE="seedmtk"
