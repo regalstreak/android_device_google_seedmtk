9fe58c8adb9f44e3a2cc680345fb3f7d179b487f
Harsh Shandilya <msfjarvis@gmail.com>
2016-07-11 16:00:47 +0530

    Remove duplicate init.sprout.rc
    sprout-common already defines and copies this file

--- seedmtk.mk
+++ seedmtk.mk
@@ -25,5 +25,4 @@ $(shell touch $OUT/obj/KERNEL_OBJ/usr/export_includes)
 $(call inherit-product, device/google/sprout-common/sprout.mk)
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/root/init.sprout.rc:root/init.sprout.rc \
     $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout
