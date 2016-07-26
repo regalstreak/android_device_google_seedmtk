68b05760638dc4727353d58506fe0078fb5700cc
Harsh Shandilya <msfjarvis@gmail.com>
2016-07-26 15:57:04 +0530

    Fix override derp
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- du.mk
+++ du.mk
@@ -13,7 +13,9 @@ PRODUCT_MODEL := Pixelv1
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="seedmtk" \
     BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys \
-    PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys" \
+    PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-a1-lava
 
 $(call inherit-product, device/google/seedmtk/seedmtk.mk)
